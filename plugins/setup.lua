c05f162f2ca2518b9483a19b3af64112f90f7c2a
Anton Borissov <anton.borissov@softwareq.ca>
2025-06-30 11:15:19 -0400

    init

--- plugins/setup.lua
+++ plugins/setup.lua
@@ -4,7 +4,7 @@
 local copy_buffers_module = require("copy-all-buffers")
 
 -- Create the keymap that calls the function from our module
-vim.keymap.set("n", "<leader>ABC", copy_buffers_module.copy_all_to_clipboard, {
+vim.keymap.set("n", "<leader>bc", copy_buffers_module.copy_all_to_clipboard, {
 	noremap = true,
 	silent = true,
 	desc = "Copy All Buffers to Clipboard",
